c6651eb733a0f6a2423fef89396e18d0f46e7ae8
Suhaas Gummalam <footballandgermany@gmail.com>
2024-09-01 16:07:45 -0400

    add notes for all AI and DE projects

--- src/components/AIEngineer.js
+++ src/components/AIEngineer.js
@@ -4,12 +4,15 @@ import SectionTemplate from "./SectionTemplate";
 
 const AIEngineer = () => {
   const projects = [
-    { name: "Project One", description: "A cool web development project.", link: "#" },
-    { name: "Project Two", description: "Another cool web development project.", link: "#" },
-    // Add more projects as needed
+    { name: "Revise Mate", description: "A system that generates a quick quiz with an interactive interface based on input text or document.", storyTime: "My mother is a veteran lecturer. One day, she told me how time-consuming it is for teachers to create new questions for every class, every year, for revision. Coincidentally, I was interested in Retrieval-Augmented Generation (RAG) at the time, so I told her to hold my glass of water—and boom! I created this project for her, and she actually uses it now. She thought I was a genius for this, bwahahaha. I have a long way to go =(", technologies: "Python, Langchain, FastAPI, Hugginface, Streamlit, ChromaDB", link: "https://github.com/overlordiam/ReviseMate" },
+    { name: "SLM Comparitive Analysis", description: "A comparison of four different Small Language Models (Mistral-7B, Zephyr_7B, Orca-7B, and Phi-2) in a question-answering task.", storyTime: "This project serves as a precursor to the Revise Mate project. During one of my internships, I was tasked with analyzing existing SLMs (Small Language Models) to determine which model performed best overall. This analysis was one of the experiments I conducted. Based on the specific use-case, I could then recommend and implement the most suitable model for the task at hand.", technologies: "Python, PyTorch, Numpy, Plotly, Langchain, Hugginface Transformers, ChromaDB", link: "https://github.com/overlordiam/SLM_comparison" },
+    { name: "Image Classification: A Concordia Competition", description: "A comprehensive codebook on hyperparameter tuning and model selection for a classification task.", storyTime: "It was a rollercoaster of emotions. Initially, my teammates and I were apathetic about the project. Then, our professor introduced a leaderboard, which didn't motivate us until we made a submission that unexpectedly catapulted us to 2nd place. Surprised by our success, the real competition began. We fluctuated between 2nd and 1st place, eventually holding the top spot until the last day when another team overtook us by less than a percent difference in accuracy. Despite this, it was an exhilarating time throughout.", technologies: "Python, PyTorch, Scikit-learn", link: "https://github.com/ajay-ramasubramanian/CIFAR-10-Challenge" },
+    { name: "Transformer from scratch", description: "Built the transformer architecture as described in the paper 'Attention is All You Need'.", storyTime: "I was familiar with the groundbreaking paper 'Attention is All You Need' and understood its theoretical aspects. But did I know how it was coded? How the authors went about each module? These were some questions I asked myself. I attempted to code it myself and was stumped from the first step. Then I realized that theoretical knowledge doesn't always translate to practical implementation. With help from external resources and videos, I was finally able to code the entire architecture, bridging the gap between theory and practice.", technologies: "Python, PyTorch, Numpy, Hugginface", link: "https://github.com/overlordiam/Transformer_from_scratch" },
+    { name: "Keyword Spotting", description: "An analysis on the ", storyTime: "", technologies: "Python, Speechbrain, PyTorch", link: "https://github.com/overlordiam/Keyword_spotting" },
+
   ];
 
-  return <SectionTemplate title="Suhaas as a Web Developer" projects={projects} />;
+  return <SectionTemplate title="The AI G.O.A.T (that's stretching it a little)" projects={projects} />;
 };
 
 export default AIEngineer;
--- src/components/SectionTemplate.js
+++ src/components/SectionTemplate.js
@@ -1,15 +1,16 @@
 // src/components/SectionTemplate.js
 import React from "react";
 
-const SectionTemplate = ({ title, projects }) => {
+const SectionTemplate = ({ title, subtitle, projects }) => {
   return (
     <div className="container mx-auto px-4 py-16">
       <h2 className="text-3xl font-bold mb-8">{title}</h2>
+      <h2 className="text-1xl font-bold mb-8">{subtitle}</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project, index) => (
           <div key={index} className="bg-gray-800 p-6 rounded hover:bg-gray-700 transition duration-300">
             <h3 className="text-xl font-semibold mb-4">{project.name}</h3>
-            <p className="mb-4">{project.description}</p>
+            <p className="mb-4"><span className="font-bold">Boring description:</span>  {project.description}</p>
             <p className="mb-4"><span className="font-bold">Story time:</span>  {project.storyTime}</p>
             <p className="mb-4"><span className="font-bold">Tech:</span>  {project.technologies}</p>
             <a href={project.link} target="_blank" rel="noreferrer" className="text-blue-400 hover:underline">
